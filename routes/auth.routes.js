3fefaa1465a82a467ca02a20c074e709deacb897
Pasindu Lakshan Gunasekara <shanugunasekara11@gmail.com>
2024-03-03 01:35:55 +0530

    Add validation middleware and schema for login endpoint

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,9 +5,12 @@ const loginController = require('../controllers/auth/login.controller');
 const registerController = require('../controllers/auth/register.controller');
 const updateController = require('../controllers/auth/update.controller');
 
+const AuthValidation = require("../middlewares/validations/auth.validations");
+const validateSchema = require("../helpers/joiValidation");
+
 const authRouter = Router();
 
-authRouter.post('/login', loginController);
+authRouter.post('/login', validateSchema({body:AuthValidation.loginSchema}), loginController);
 authRouter.post('/register', registerController);
 authRouter.put('/update/:id', updateController);
 
