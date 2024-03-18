f83b3f931622f44ea05988377d8cb7dfc9487e5a
senuravihanjayadeva <senurajayadeva@gmail.com>
2024-03-18 19:38:37 +0530

    Remove user form my team

--- routes/my-team.routes.js
+++ routes/my-team.routes.js
@@ -2,11 +2,14 @@ const { Router } = require('express');
 const { sendAccessRequest } = require('../controllers/account-setting/my-team/send-access-request.controller');
 const getMySupervisorsController = require('../controllers/account-setting/my-team/getMySupervisors.controller');
 const getMyAgentsController = require('../controllers/account-setting/my-team/getMyAgents.controller');
+const removeAccessFromMyTeam = require('../controllers/account-setting/my-team/removeAccessFromMyTeam');
+
 
 const myTeamRouter = Router();
 
 myTeamRouter.post('/send-access-request', sendAccessRequest);
 myTeamRouter.get('/get-my-supervisors/:user', getMySupervisorsController);
 myTeamRouter.get('/get-my-agents/:user', getMyAgentsController);
+myTeamRouter.put('/remove-access-request/:user', removeAccessFromMyTeam);
 
 module.exports = myTeamRouter;
