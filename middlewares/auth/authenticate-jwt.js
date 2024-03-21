8696cb7bbca45c0b1e3f2cea0d784b5018c377ed
senuravihanjayadeva <senurajayadeva@gmail.com>
2024-03-21 15:06:33 +0530

    Authentication implementation

--- middlewares/auth/authenticate-jwt.js
+++ middlewares/auth/authenticate-jwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateJWT = (req, res, next) => {
-    const token = req.headers['authorization'];
+    const token = req.headers['x-auth-token'];
     if (!token) {
         return res.status(401).json({ message: 'Authentication failed: No token provided' });
     }
